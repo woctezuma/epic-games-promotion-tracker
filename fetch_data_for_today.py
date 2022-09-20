c0566a296d8db26abf53c373ff71bef381ac31b7
Wok <woctezuma@users.noreply.github.com>
2022-09-21 00:23:02 +0200

    Add script to fetch data for today

---
+++ fetch_data_for_today.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from src.download_utils import download_upcoming_promotions
+from src.export_utils import write_markdown_files
+from src.json_utils import save_json, load_json
+from src.time_utils import get_fname_for_today
+
+
+def main():
+    output_fname = get_fname_for_today()
+    Path(output_fname).parent.mkdir(parents=True, exist_ok=True)
+
+    requires_to_download_json = not Path(output_fname).exists()
+    requires_to_update_markdown = requires_to_download_json
+
+    if requires_to_download_json:
+        print('Updating JSON.')
+        data = download_upcoming_promotions()
+        save_json(data, output_fname, prettify=True)
+    else:
+        data = load_json(output_fname)
+
+    if requires_to_update_markdown:
+        print('Updating Markown.')
+        write_markdown_files(data)
+
+    return
+
+
+if __name__ == '__main__':
+    main()
