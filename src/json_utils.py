48cfdf6f30862e4d4daa77f4e6c5c8d96d99adb4
Wok <woctezuma@users.noreply.github.com>
2022-09-21 00:21:02 +0200

    Add JSON utils

---
+++ src/json_utils.py
@@ -0,0 +1,15 @@
+import json
+
+
+def load_json(fname):
+    with open(fname, 'r', encoding='utf8') as f:
+        data = json.load(f)
+    return data
+
+
+def save_json(data, fname, prettify=False):
+    with open(fname, 'w', encoding='utf8') as f:
+        if prettify:
+            json.dump(data, f, indent=2, sort_keys=True)
+        else:
+            json.dump(data, f)
