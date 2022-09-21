175828180a78cb1b50b3761fc382865e158553d8
Wok <woctezuma@users.noreply.github.com>
2022-09-21 11:24:41 +0200

    Save temporary store data to disk

--- src/download_utils.py
+++ src/download_utils.py
@@ -1,3 +1,5 @@
+from src.data_utils import STORE_DATA_FNAME
+from src.json_utils import save_json
 from src.parse_store_data import get_total_num_store_elements, get_store_elements
 from src.parse_store_element import convert_to_promos
 from src.query_store_data import to_store_data
@@ -5,7 +7,7 @@ from src.query_store_data import to_store_data
 MAX_STEP_SIZE = 1000
 
 
-def download_store_elements():
+def download_store_elements(save_store_data_to_disk=True):
     dummy_store_data = to_store_data(cursor=0, step=1)
     num_elements = get_total_num_store_elements(dummy_store_data)
 
@@ -18,10 +20,13 @@ def download_store_elements():
 
         all_store_elements += store_elements
 
+    if save_store_data_to_disk:
+        save_json(all_store_elements, STORE_DATA_FNAME, prettify=True)
+
     return all_store_elements
 
 
-def download_upcoming_promotions():
-    store_elements = download_store_elements()
+def download_upcoming_promotions(save_store_data_to_disk=True):
+    store_elements = download_store_elements(save_store_data_to_disk)
     all_upcoming_promotions = convert_to_promos(store_elements)
     return all_upcoming_promotions
