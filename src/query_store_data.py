d2b6fac158413184d11a8f8d299500a4bec33d3d
Wok <woctezuma@users.noreply.github.com>
2022-09-21 10:44:10 +0200

    Fetch more meta-data about slugs

--- src/query_store_data.py
+++ src/query_store_data.py
@@ -3,11 +3,11 @@ from src.api import send_post_request_to_api
 
 def get_params_to_query_store_data(cursor, step):
     query_str = "{Catalog {searchStore" + f'(start: {cursor}, count: {step}, country: "FR")' + " {elements {"
-    meta_data_str = "title catalogNs {mappings {pageSlug}} promotions {upcomingPromotionalOffers {"
-    promotion_str = "promotionalOffers {startDate endDate discountSetting {discountPercentage} }"
-    paging_str = "}}} paging {count total} }}}"
+    slug_str = "productSlug urlSlug offerMappings {pageSlug} title catalogNs {mappings {pageSlug}} promotions {"
+    promo_str = "upcomingPromotionalOffers {promotionalOffers {startDate endDate discountSetting {discountPercentage}}}"
+    paging_str = "}} paging {count total} }}}"
 
-    params = {"query": query_str + meta_data_str + promotion_str + paging_str}
+    params = {"query": query_str + slug_str + promo_str + paging_str}
 
     return params
 
