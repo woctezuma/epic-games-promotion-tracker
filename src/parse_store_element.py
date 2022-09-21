0e2b23defb19a40d56afbae54df8ca6e14724052
Wok <woctezuma@users.noreply.github.com>
2022-09-21 23:41:31 +0200

    Minor refactoring: move some functions used to parse promotions

--- src/parse_store_element.py
+++ src/parse_store_element.py
@@ -1,20 +1,5 @@
+from src.promotion_utils import get_upcoming_promotional_offers, to_promo_element
 from src.slug_utils import to_slug
-
-
-def get_upcoming_promotional_offers(store_element):
-    try:
-        promotion_list = store_element["promotions"]["upcomingPromotionalOffers"]
-    except TypeError:
-        promotion_list = []
-
-    if len(promotion_list) > 0:
-        promotional_offers = promotion_list[0]["promotionalOffers"]
-        if len(promotion_list) > 1:
-            print(promotion_list)  # TODO
-    else:
-        promotional_offers = None
-
-    return promotional_offers
 
 
 def to_upcoming_promotion(store_element):
@@ -30,22 +15,6 @@ def to_upcoming_promotion(store_element):
         slug = to_slug(store_element)
 
         promo_element = to_promo_element(slug, store_element["title"], first_promotional_offer)
-
-    return promo_element
-
-
-def to_promo_element(slug, title, promotional_offer):
-    # NB: the field name is confusing: this is the ratio between the discounted price and the base price.
-    ratio = promotional_offer["discountSetting"]["discountPercentage"]
-
-    promo_element = {
-        "slug": slug,
-        "title": title,
-        "startDate": promotional_offer["startDate"],
-        "endDate": promotional_offer["endDate"],
-        "discountPercentage": 100 - ratio,
-        "isFree": bool(ratio == 0)
-    }
 
     return promo_element
 
---
+++ src/promotion_utils.py
@@ -0,0 +1,28 @@
+def get_upcoming_promotional_offers(store_element):
+    try:
+        promotion_list = store_element["promotions"]["upcomingPromotionalOffers"]
+    except TypeError:
+        promotion_list = []
+
+    if len(promotion_list) > 0:
+        promotional_offers = promotion_list[0]["promotionalOffers"]
+    else:
+        promotional_offers = None
+
+    return promotional_offers
+
+
+def to_promo_element(slug, title, promotional_offer):
+    # NB: the field name is confusing: this is the ratio between the discounted price and the base price.
+    ratio = promotional_offer["discountSetting"]["discountPercentage"]
+
+    promo_element = {
+        "slug": slug,
+        "title": title,
+        "startDate": promotional_offer["startDate"],
+        "endDate": promotional_offer["endDate"],
+        "discountPercentage": 100 - ratio,
+        "isFree": bool(ratio == 0)
+    }
+
+    return promo_element
