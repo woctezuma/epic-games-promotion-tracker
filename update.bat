fa05f710ae14d71a3d982810553f77cfd9608232
Wok <woctezuma@users.noreply.github.com>
2022-10-01 09:56:04 +0200

    Edit batch file as JSON file is renamed and overwritten

--- update.bat
+++ update.bat
@@ -3,8 +3,8 @@ python fetch_data_for_today.py
 :: https://stackoverflow.com/a/72000317/376454
 chcp 65001>nul
 
-git add data/20*/*
-git commit -m "🗃️ Add JSON data snapshot"
+git add data/promotion_data.json
+git commit -m "🗃️ Update JSON data snapshot"
 
 git add docs/by_*.md
 git commit -m "📄 Update Markdown docs"
