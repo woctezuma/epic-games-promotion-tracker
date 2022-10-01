314d2e66edbea0d54a97b8b7ca4f1d44118c45b2
Wok <woctezuma@users.noreply.github.com>
2022-10-01 18:33:04 +0200

    Ensure destination folder exists when saving JSON file

--- src/json_utils.py
+++ src/json_utils.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 
 def load_json(fname):
@@ -8,6 +9,8 @@ def load_json(fname):
 
 
 def save_json(data, fname, prettify=False):
+    Path(fname).parent.mkdir(parents=True, exist_ok=True)
+
     with open(fname, 'w', encoding='utf8') as f:
         if prettify:
             json.dump(data, f, indent=2, sort_keys=True)
