bae6de64eb30af40a603882e077f8718a5b653bd
Wok <woctezuma@users.noreply.github.com>
2022-10-26 22:07:07 +0200

    Expose the filename of the Markdown doc with free games

--- src/export_utils.py
+++ src/export_utils.py
@@ -3,6 +3,7 @@ from src.markdown_utils import format_data_as_markdown
 from src.time_utils import formate_dates_for_display
 
 OUTPUT_FOLDER = 'docs'
+FREE_GAME_FNAME = f"{OUTPUT_FOLDER}/by_free_game.md"
 
 
 def write_lines_to_disk(lines, fname):
@@ -22,7 +23,7 @@ def write_markdown_files(data):
     data = formate_dates_for_display(data)
 
     sorted_data = sorted(filter_in_free_games(data), key=lambda x: (x["startDate"], x["title"]))
-    write_data_to_disk(sorted_data, f"{OUTPUT_FOLDER}/by_free_game.md")
+    write_data_to_disk(sorted_data, f"{FREE_GAME_FNAME}")
 
     sorted_data = sorted(data, key=lambda x: (x["slug"], x["startDate"]))
     write_data_to_disk(sorted_data, f"{OUTPUT_FOLDER}/by_game_slug.md")
