e566582f86e01888065549adf160fb22c175b2dd
Wok <woctezuma@users.noreply.github.com>
2022-10-26 22:05:32 +0200

    Add Discord webhook

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,3 +1,10 @@
+from src.json_utils import load_json_failsafe
+
 DATA_FOLDER = 'data'
 STORE_DATA_FNAME = f"{DATA_FOLDER}/store_data.json"
 PROMOTION_DATA_FNAME = f"{DATA_FOLDER}/promotion_data.json"
+DISCORD_WEBHOOK_FNAME = f'{DATA_FOLDER}/discord_webhook.json'
+
+
+def load_discord_webhook():
+    return load_json_failsafe(f"{DISCORD_WEBHOOK_FNAME}")
