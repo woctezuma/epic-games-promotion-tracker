868de9fc44fedc3564b42339a7fa8c2818163ba5
Wok <woctezuma@users.noreply.github.com>
2022-10-27 16:51:30 +0200

    Find Discord header and webhook ID with keyword in the script

--- post_to_discord.py
+++ post_to_discord.py
@@ -1,10 +1,10 @@
-from src.discord_utils import post_git_diff_to_discord
+from src.discord_utils import post_git_diff_to_discord_using_keyword
 from src.export_utils import FREE_GAME_FNAME
-from src.webhook_utils import DISCORD_FREE_HEADER
+from src.webhook_utils import WEBHOOK_KEYWORD_FREE
 
 
 def main():
-    post_git_diff_to_discord(fname=FREE_GAME_FNAME, header=DISCORD_FREE_HEADER)
+    post_git_diff_to_discord_using_keyword(fname=FREE_GAME_FNAME, webhook_keyword=WEBHOOK_KEYWORD_FREE)
 
     return
 
