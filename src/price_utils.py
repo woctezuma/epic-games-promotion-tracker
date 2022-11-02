3d83b721807096c1761dd7d892527de74d4a89cf
Wok <woctezuma@users.noreply.github.com>
2022-11-02 19:31:28 +0100

    Allow to format prices for display

--- src/price_utils.py
+++ src/price_utils.py
@@ -1,3 +1,6 @@
+from src.markdown_utils import PRICE_FIELDS
+
+
 def to_price(store_element):
     try:
         price = store_element["price"]["totalPrice"]["originalPrice"]
@@ -5,3 +8,29 @@ def to_price(store_element):
         price = None
 
     return price
+
+
+def format_prices_for_display(data):
+    for entry in data:
+        format_prices_in_dict(entry)
+
+    return data
+
+
+def format_prices_in_dict(entry):
+    for k in PRICE_FIELDS:
+        entry[k] = prepare_price_for_display(entry[k])
+    return
+
+
+def prepare_price_for_display(price_in_cents, currency='€'):
+    if price_in_cents is None:
+        output = 'N/A'
+    else:
+        price_in_euros = to_units(price_in_cents)
+        output = f"{price_in_euros} {currency}".replace('.', ',')
+    return output
+
+
+def to_units(price_in_cents):
+    return price_in_cents / 100
