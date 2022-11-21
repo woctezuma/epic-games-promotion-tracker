e19da402169efc342ecf147fb207150d8304dc92
Wok <woctezuma@users.noreply.github.com>
2022-11-21 15:26:26 +0100

    Linting: remove unused code in time utils

--- src/time_utils.py
+++ src/time_utils.py
@@ -1,25 +1,4 @@
-from datetime import datetime
-
-from src.data_utils import DATA_FOLDER
 from src.markdown_utils import MISSING_DATA, TIME_FIELDS
-
-
-def get_current_date():
-    return datetime.now()
-
-
-def get_folder_name_for_specific_day(date):
-    return f"{DATA_FOLDER}/{date.year}/{date.month:02}"
-
-
-def get_fname_for_specific_day(date):
-    folder_name = get_folder_name_for_specific_day(date)
-    return f"{folder_name}/{date.day:02}.json"
-
-
-def get_fname_for_today():
-    date = get_current_date()
-    return get_fname_for_specific_day(date)
 
 
 def format_dates_for_display(data):
