3215e11ea4825fdac4a29bd904da9124b0348d06
Wok <woctezuma@users.noreply.github.com>
2022-12-25 00:30:05 +0100

    Download all the store data instead of calling main()

--- fetch_data_for_today.py
+++ fetch_data_for_today.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from src.data_utils import PROMOTION_DATA_FNAME
-from src.download_utils import download_upcoming_promotions
+from src.download_utils import download_upcoming_promotions, download_store_elements
 from src.export_utils import write_markdown_files
 from src.json_utils import load_json, save_json
 from src.utils import unique
@@ -24,4 +24,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    all_store_elements = download_store_elements(include_dlc=False, save_store_data_to_disk=True)
