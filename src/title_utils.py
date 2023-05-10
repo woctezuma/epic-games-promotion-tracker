b6c41db430ca674fa7f8da10782ebc31580e1fb9
Wok <woctezuma@users.noreply.github.com>
2023-05-10 22:35:21 +0200

    Escape ambiguous characters, mistakenly interpreted as column separator

--- src/title_utils.py
+++ src/title_utils.py
@@ -18,6 +18,7 @@ def prepare_title_for_display(title: str) -> str:
         output = MISSING_DATA
     else:
         output = remove_spurious_spaces(title)
+        output = escape_ambiguous_characters(output)
     return output
 
 
@@ -26,3 +27,13 @@ def remove_spurious_spaces(title: str) -> str:
     Reference: https://stackoverflow.com/a/2077944/376454
     """
     return " ".join(title.split())
+
+
+def escape_ambiguous_characters(title: str) -> str:
+    """This escapes characters which could be mistakenly interpreted as column separator.
+    For instance: "Filthy Animals | Heist Simulator".
+    Reference: https://store.epicgames.com/p/filthy-animals-f6c65c
+    """
+    ambiguous_character = "|"
+    escape_character = "\\"
+    return title.replace(ambiguous_character, escape_character + ambiguous_character)
